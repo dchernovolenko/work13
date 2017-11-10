5a0655e0ccb6e774dee48cb62ad9ffcb8dceb571
dchernovolenko <dchernovolenko@stuy.edu>
2017-11-10 13:46:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    api = urllib2.urlopen('https://api.nasa.gov/planetary/apod?api_key=eqrkm70XPoAxvxbcrHZnC37LAB5dXa5odR9cdWYd')
+    api = urllib2.urlopen('http://xkcd.com/info.0.json')
     api_dict = json.loads(api.read())
-    return render_template('template.html', image=api_dict['url'], title=api_dict["title"], desc=api_dict["explanation"])
+    return render_template('template.html', image=api_dict['img'], title=api_dict["title"], desc=api_dict["alt"], day = api_dict["day"], month = api_dict["month"], year = api_dict["year"])
 
 if __name__ == '__main__':
   app.debug = True
